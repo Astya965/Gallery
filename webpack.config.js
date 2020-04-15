7f85a460c2684a26ef5c129fedecee539f627817
astya965 <astya965@gmail.com>
2020-04-15 06:41:01 +0300

    Add: Testing webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require(`path`);
+const pathMain = path.join(__dirname, `public`);
+
+module.exports = {
+  entry: `./source/js/index.js`,
+  output: {
+    filename: `bundle.js`,
+    path: pathMain
+  },
+  devServer: {
+    contentBase: pathMain,
+    open: false,
+    inline: false,
+    port: 1881,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: `babel-loader`,
+        },
+      }
+    ],
+  },
+  devtool: `source-map`,
+};
