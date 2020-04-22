a3df0313d72789b95d3efc9140a1c0d7281d6203
astya965 <astya965@gmail.com>
2020-04-23 00:23:38 +0300

    Add: Mobile styles were added for Timeline component

--- source/js/components/timeline/timeline.jsx
+++ source/js/components/timeline/timeline.jsx
@@ -1,5 +1,7 @@
 import React, {PureComponent} from "react";
 
+const MOBILE_WIDTH = 780;
+
 class Timeline extends PureComponent {
   constructor(props) {
     super(props);
@@ -16,9 +18,9 @@ class Timeline extends PureComponent {
 
   render() {
     return (
-      <div className="header__timeline timeline">
+      <div className={`header__timeline timeline`}>
       <button className="timeline__toggle" type="button" onClick={this._onButtonToggle}>Timeline ⟶</button>
-      <ul className="timeline__list">
+      <ul className={`timeline__list ${document.documentElement.clientWidth <= MOBILE_WIDTH ? `visually-hidden` : ``}`}>
         {this.props.timeline.map((year, i) => (
           <li className="timeline__date" key={year + `-${i}`}>
             <a className="timeline__link" href="#">{year}</a>
